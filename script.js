2226bec9d91fff514a2ada15fbf5a8197d4d1a2d
anshuman120 <anshumangarg120@gmail.com>
2023-08-27 17:12:10 +0530

    change string name

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 /** @format */
 
-let string = "";
+let input = "";
 let buttons = document.querySelectorAll(".button");
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
-      string = eval(string);
-      document.querySelector("input").value = string;
+      input = eval(input);
+      document.querySelector("input").value = input;
     } else if (e.target.innerHTML == "C") {
-      (string = ""), (document.querySelector("input").value = string);
+      (input = ""), (document.querySelector("input").value = input);
     } else if (e.target.innerHTML == "%") {
-      string = parseFloat(string) / 100;
-      document.querySelector("input").value = string;
+      input = parseFloat(input) / 100;
+      document.querySelector("input").value = input;
     }
     else if (e.target.innerHTML == "%..") {
       // Prompt the user to enter the percentage
       let percentage = parseFloat(prompt("Enter the percentage:"));
 
       // Calculate the percentage of the input number
-      string = (parseFloat(string) * percentage) / 100;
-      document.querySelector("input").value = string;
+      input = (parseFloat(input) * percentage) / 100;
+      document.querySelector("input").value = input;
     }
     else {
       console.log(e.target);
-      string = string + e.target.innerHTML;
-      document.querySelector("input").value = string;
+      input = input + e.target.innerHTML;
+      document.querySelector("input").value = input;
     }
   });
 });
